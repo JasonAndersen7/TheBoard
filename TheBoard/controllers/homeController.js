6eaed7c8ce3fcc47a651e3fbb9d38bd84933de56
Andersen, Jason <jason.andersen@terumobct.com>
2019-06-25 09:05:12 -0600

    Adding Service

--- TheBoard/controllers/homeController.js
+++ TheBoard/controllers/homeController.js
@@ -1,11 +1,19 @@
 (function (homeController) {
 
+    var data = require("../data");
+
     homeController.init = function (app) {
 
         app.get("/", function (req, res) {
+            //res.send("<html><body><h1>Express</h1></body></html>");
 
-            res.render("index", { title: "Express + Vash", greeting: "Hello Jason" });
+            data.getNoteCategories(function (err, results) {
+
+                res.render("index", { title: "The Board", error: err, categories: results, greeting: "Hello Jason" });
+
+            });
         });
+
     };
 
 })(module.exports);
